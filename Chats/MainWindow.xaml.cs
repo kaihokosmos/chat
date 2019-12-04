f2a7ffb200c3f8a6657d3916a667a2ce31e7e46f
Ploch Stefan <Stefan.Ploch@training.lug-ag.de>
2019-12-04 13:55:26 +0100

    initial commit

---
+++ Chats/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace Chats
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ConnectButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				IPAddress ipAddress = IPAddress.Parse(IpAddressInput.Text); // Textbox
+				TcpClient client = new TcpClient();
+				client.Connect(ipAddress, 5000);
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Ungültige IP-Adresse");
+			}
+			catch (SocketException)
+			{
+				MessageBox.Show("Server nicht erreichbar");
+			}
+		}
+	}
+}
